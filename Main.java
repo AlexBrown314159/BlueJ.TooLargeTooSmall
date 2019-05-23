0c51dd6b9ecf878f7152ba0e8d04ffd2c72858b2
Alex Brown <alexbrown@comcast.net>
2019-05-23 14:07:15 -0400

    Updated files.

--- Main.java
+++ Main.java
@@ -1,33 +1,79 @@
-
+import java.util.Scanner;
+import java.util.Random;
 /**
- * Write a description of class Main here.
+ * This code picks a random number between rangeMin and rangeMax inclusive.
+ * The player must guess the number when given clues of "Large!" or "Too Small!".
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Alex Brown)
+ * @version (2019-05-22)
  */
 public class Main
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private int rangeMin = 1;
+    private int rangeMax = 100;
+    private int actualNumber = 0;
+    private int myGuess = 0;
+    private int numberOfGuesses = 0;
 
     /**
      * Constructor for objects of class Main
      */
-    public Main()
+    public void Main()
     {
-        // initialise instance variables
-        x = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("I'm thinking of an integer between " + rangeMin);
+        System.out.println(" and " + rangeMax + " inclusive.");
+        System.out.println("");
+        
+        // Get the number
+        actualNumber = randomNumber(rangeMin, rangeMax);
+        
+        Boolean playOn = true;
+        while (playOn) {
+     
+            System.out.print("Take a Guess: ");
+  
+            myGuess = sc.nextInt();
+            numberOfGuesses++;
+        
+            if (myGuess < actualNumber) {
+                System.out.println("Too Small!");
+                System.out.println("");
+            }
+            else if (myGuess > actualNumber) {
+                System.out.println("Too Large!");
+                System.out.println("");
+            }
+            else {
+                System.out.println("Correct!");
+                System.out.println("You guessed my number in only " + numberOfGuesses + " tries!");
+                System.out.println("--------------------------------------------------");
+                playOn = false;
+        }
+    }
+
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * Method to generate a random integer 
+     * between rMin and rMax inclusive
      */
-    public int sampleMethod(int y)
+    public int randomNumber(int rMin, int rMax)
     {
-        // put your code here
-        return x + y;
+        Random randMaker = new Random();
+    
+        int randNum = randMaker.nextInt(rMax - rMin + 1) + rMin;
+      
+        return randNum;
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
